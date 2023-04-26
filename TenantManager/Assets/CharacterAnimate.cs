926ba52ecc9f3c2d75c213acfce5558bd1624c0a
issygrr <61847671+issygrr@users.noreply.github.com>
2023-04-26 12:21:57 +0100

    tm

---
+++ TenantManager/Assets/CharacterAnimate.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CharacterAnimate : MonoBehaviour
+{
+    [SerializeField] Animator animator;
+    NavMeshAgent agent;
+
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+    private void Update()
+    {
+        animator.SetBool("Moving", agent.velocity.magnitude > 0.1f);
+    }
+}
